5c379f1b3785fd42251b4b3045888220c5267813
thisanches07 <thi.sanches@hotmail.com>
2021-08-20 08:56:21 -0300

    GetById

--- src/main/java/com/rest/api/controller/SchoolController.java
+++ src/main/java/com/rest/api/controller/SchoolController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,4 +51,10 @@ public class SchoolController {
     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(school.getId()).toUri();
     return ResponseEntity.created(uri).body(school);                                                         
 }
+    @GetMapping("/{id}")
+    public ResponseEntity<SchoolDTO> getSchoolById(@PathVariable long id){
+        SchoolDTO school = service.getSchoolById(id);
+        return ResponseEntity.ok(school);
+    }
+
 }
--- src/main/java/com/rest/api/service/SchoolService.java
+++ src/main/java/com/rest/api/service/SchoolService.java
@@ -1,5 +1,7 @@
 package com.rest.api.service;
 
+import java.util.Optional;
+
 import com.rest.api.dto.SchoolDTO;
 import com.rest.api.dto.SchoolInsertDTO;
 import com.rest.api.entity.School;
@@ -8,7 +10,9 @@ import com.rest.api.repository.SchoolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 @Service
 public class SchoolService {
 
@@ -24,5 +28,11 @@ public class SchoolService {
         School entity = new School(school);
         entity = repo.save(entity);
         return new SchoolDTO(entity);
+    }
+
+    public SchoolDTO getSchoolById(long id){
+        Optional<School> op = repo.findById(id);
+        School school = op.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"School doesn't exsist"));
+        return new SchoolDTO(school);
     }
 }
